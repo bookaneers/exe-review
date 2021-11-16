9b167bc4a4b1878dc745c72e9319f887e409d8a1
Fernando Figueiredo <bookaneers@gmail.com>
2021-11-16 14:51:06 -0700

    Completed javascript-primitives-and-variables

---
+++ 022-javascript-primitives-and-variables/main.js
@@ -0,0 +1,16 @@
+var fullName = 'Fernando Figueiredo';
+var isCool = true;
+var totalPets = 2;
+console.log(`value of fullName: ${fullName}`);
+console.log(`value of isCool: ${isCool}`)
+console.log(`value of totalPets: ${totalPets}`);
+console.log(`typeof fullName: ${typeof fullName}`);
+console.log(`typeof isColl: ${typeof isColl}`);
+console.log(`typeof totalPets: ${typeof totalPets}`);
+console.log
+var unknown = null;
+console.log(`value of null: ${null}`);
+console.log(`typeof null: ${typeof null}`);
+var never;
+console.log(`value of never: ${never}`);
+console.log(`typeof never: ${typeof never}`);
